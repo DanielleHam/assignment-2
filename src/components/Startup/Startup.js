5e7b9a54cb3db6dd51a1e12224392b5dc927a1c8
MSmed <mattias.smedman@gmail.com>
2023-01-20 10:12:57 +0100

    feat: Add sign in input box and button
    WIP #1

--- src/components/Startup/Startup.js
+++ src/components/Startup/Startup.js
@@ -2,22 +2,36 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "../../styling/startup.css";
-import LogoHello from "../../images/Logo-Hello.png";
-
+import LogoHello from "../../images/HelloCloud.png";
+import Button from "react-bootstrap/Button"
 
 const Startup = () => 
 {
     return (
-        <Container fluid className="bg">
-            <Row className="welcome align-items-center mt-5 mb-5">
-                <Col>
-                <img src={LogoHello} alt="Logo hello" />
+        <>
+            <Container fluid className="bg">
+                <Row className="welcome align-items-center mt-5 mb-5">
+                    <Col>
+                        <img src={LogoHello} alt="Logo hello" />
+                    </Col>
+                    <Col>
+                        <h1>Welcome<br />to<br />Lost in translation</h1>
+                    </Col>
+                </Row>
+                
+            </Container>
+            <Row className="justify-content-center align-items-center signIn">
+                <Col className="col-3">
+                    <Container className="nameContainer d-flex align-items-left rounded-5 shadow">
+                        <input className="nameInput mt-3 mb-3" type="text" placeholder="Enter your name"></input>
+                    </Container>
                 </Col>
-                <Col>
-                    <h1>Welcome<br />to<br />Lost in translation</h1>
+                <Col className="col-2">
+                    <Button className="signInButton p-3 px-5 rounded-5 shadow">Sign in</Button>
                 </Col>
             </Row>
-        </Container>
+        </>
+        
     );
 }
 
