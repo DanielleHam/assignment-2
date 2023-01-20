2be048a1baf6ba2d6612ec2a17b8692b65d7962b
MSmed <mattias.smedman@gmail.com>
2023-01-20 08:28:48 +0100

    feat: Add background to landing page background
    Adds a background and border top to the landing page.
    WIP #1

---
+++ src/components/Startup/Startup.js
@@ -0,0 +1,12 @@
+import Container from "react-bootstrap/Container";
+import "../../styling/startup.css"
+const Startup = () => 
+{
+    return (
+        <Container fluid className="bg">
+            <p>Hello</p>
+        </Container>
+    );
+}
+
+export default Startup;
