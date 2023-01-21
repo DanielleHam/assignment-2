d05892fadfdfbcabe58f9eb6f08c6022a2b18772
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2023-01-21 17:02:41 +0100

    feat: Translation page design
    added container and shadow on input text, button and container for images.

--- src/components/Translation.js
+++ src/components/Translation.js
@@ -1,14 +1,19 @@
 import "../styling/Translation.css";
 import Button from 'react-bootstrap/Button';
+import { Card } from "react-bootstrap";
 
 function Translation() {
   return (
     <>
     <hr/>
-    <div className="maintop">
+      <div className="maintop">
         <input type="text" className = "customTextInput" placeholder="what do you want to say?"></input>
         <Button variant="flat" size = "xxl">Translate</Button>{' '}
-    </div></>
+      </div>
+      <div className ="container">
+        {/* put images here */}
+      </div>
+      </>
   );
 }
 export default Translation;
