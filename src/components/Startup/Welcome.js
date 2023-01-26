55b528fb1b15b9df6f0bf486dc58062afc6436e6
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2023-01-26 16:31:28 +0100

    feat: welcome image animation
    small bounce

--- src/components/Startup/Welcome.js
+++ src/components/Startup/Welcome.js
@@ -2,27 +2,31 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import LogoHello from "../../images/HelloCloud.png";
-
+import "animate.css";
 
 const Welcome = () => {
-    return (
-        <Container fluid className="bg">
-            <Row className="welcome align-items-center mt-5 mb-5">
-                <Col>
-                    <img src={LogoHello} alt="Logo hello" />
-                </Col>
-                <Col>
-                    <h1>
-                        Welcome
-                        <br />
-                        to
-                        <br />
-                        Lost in translation
-                    </h1>
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+  return (
+    <Container fluid className="bg">
+      <Row className="welcome align-items-center mt-5 mb-5">
+        <Col>
+          <img
+            className="animate__animated animate__bounce"
+            src={LogoHello}
+            alt="Logo hello"
+          />
+        </Col>
+        <Col>
+          <h1>
+            Welcome
+            <br />
+            to
+            <br />
+            Lost in translation
+          </h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Welcome;
